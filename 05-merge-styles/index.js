d4b75c20c195e1ed46b11760ae1d479bccc515f8
michaelsmorgon <smorgon@int.pl>
2023-05-08 09:13:05 +0200

    refactor: remove debug info

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -8,7 +8,7 @@ fs.readdir(dirStylesPath, (err, files) => {
   files.forEach((file) => {
     const filePath = path.join(dirStylesPath, file);
     const ext = path.extname(filePath);
-console.log(ext);
+
     if (ext === '.css') {
       const readStream = fs.createReadStream(filePath);
       readStream.on('data', (data) => {
